a2fafaed0be3de5ea9d9b9ec896e22dfb010d246
Hernan Alvarado <hernanvid123@gmail.com>
2025-01-13 15:33:56 -0500

    chore: set up next app
    - Install `prettier`
    - Install `@halvaradop/ui-button`
    - Remove unnecessary code
    - Create `lib` and `ui` folders

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@halvaradop/ui-*/**/*.{js}",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+} satisfies Config
